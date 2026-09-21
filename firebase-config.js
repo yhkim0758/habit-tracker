2490fd140695b2e65037b9a381ab5825310f3373
yhkim0758 <yhkim0758@gmail.com>
2026-09-21 13:34:06 +0900

    feat: Firebase 설정값 적용

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,7 @@
-// Firebase 콘솔에서 복사한 웹 앱 설정값을 여기에 붙여 넣습니다.
-// 값이 비어 있으면 동기화 기능은 숨겨지고 앱은 이 기기에서만 저장합니다.
+// Firebase 웹앱 설정값 (공개되어도 안전한 값. 데이터 보호는 Firestore 보안 규칙이 담당)
 export default {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  appId: ""
+  apiKey: "AIzaSyCzjZAuxaYyAlSy663Gid-2Mfzc0elr6hs",
+  authDomain: "habit-tracker-6cace.firebaseapp.com",
+  projectId: "habit-tracker-6cace",
+  appId: "1:823324225601:web:c7b9a25650a4f1971256c0"
 };
